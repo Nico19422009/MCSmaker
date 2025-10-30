bec4b6b87fb1831a48f8d165b8d1c4e5db57c224
Nico J <147213029+Nico19422009@users.noreply.github.com>
2025-10-30 17:40:48 +0100

    bruh forgot

--- manager.py
+++ manager.py
@@ -19,7 +19,7 @@ MANIFEST_URL = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
 
 # ------- Self-update config ----------
 APP_NAME = "MCSmaker"
-CURRENT_VERSION = "1.5.5"  # Keep in sync with version.txt in the repo
+CURRENT_VERSION = "1.5.6"  # Keep in sync with version.txt in the repo
 
 # RAW GitHub URLs
 REMOTE_MANAGER_URL = "https://raw.githubusercontent.com/Nico19422009/MCSmaker/main/manager.py"
