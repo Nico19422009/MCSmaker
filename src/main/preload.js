798dde4b0053ef7a0a65d0bcfa32d83c3087b1e3
Nico J <147213029+Nico19422009@users.noreply.github.com>
2026-02-06 17:32:20 +0100

    Remove legacy Python manager

---
+++ src/main/preload.js
@@ -0,0 +1,9 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('mcsmaker', {
+  listServers: () => ipcRenderer.invoke('servers:list'),
+  createServer: (payload) => ipcRenderer.invoke('servers:create', payload),
+  startServer: (serverId) => ipcRenderer.invoke('servers:start', serverId),
+  stopServer: (serverId) => ipcRenderer.invoke('servers:stop', serverId),
+  listVersions: () => ipcRenderer.invoke('versions:list')
+});
